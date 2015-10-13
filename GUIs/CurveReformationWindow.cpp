94592e05584662ad9b03c891c143f360843ad66b
wangtseng <cheng.wang@siat.ac.cn>
2015-10-13 23:17:08 +0800

    vincent

--- GUIs/CurveReformationWindow.cpp
+++ GUIs/CurveReformationWindow.cpp
@@ -4,8 +4,6 @@ CurveReformationWindow::CurveReformationWindow(QWidget *parent) : QWidget(parent
 {
     this->setFixedSize(800,600);
     this->setWindowTitle("Curved Planar Reformation");
-
-
 }
 
 CurveReformationWindow::~CurveReformationWindow()
