0d8c11a76123fb52e69f99f35af0bcce81788e58
Oliver-King <wangchengstyle@163.com>
2015-10-20 14:12:50 +0800

    wangcheng

--- GUIs/GuidewareTrackingWindow.cpp
+++ GUIs/GuidewareTrackingWindow.cpp
@@ -14,6 +14,8 @@ GuidewareTrackingWindow::GuidewareTrackingWindow(int x,
     this->setWindowOpacity(1);
     this->setMouseTracking(true);
     this->setAutoFillBackground(true);
+    this->constructionIHM();
+
 }
 
 //!----------------------------------------------------------------------------------------------------
@@ -32,4 +34,49 @@ void GuidewareTrackingWindow::displayWindow(){
     this->resize(this->width, this->height);
     this->move(this->x, this->y);
     this->show();
+}
+void GuidewareTrackingWindow::constructionIHM(){
+    this->nameBar = new QWidget();
+    this->nameBar->setStyleSheet("background-color:red");
+
+    this->vtkDisplayBar = new QWidget();
+    this->vtkDisplayBar->setStyleSheet("background-color:green");
+
+    this->surgeryID = new QLabel();
+    this->patientName = new QLabel();
+    this->surgeryDate = new QLabel();
+
+    this->surgeryInformation = new QWidget();
+    this->surgeryInformationLayout = new QVBoxLayout(this->surgeryInformation);
+    this->surgeryInformationLayout->addWidget(surgeryID);
+    this->surgeryInformationLayout->addWidget(patientName);
+    this->surgeryInformationLayout->addWidget(surgeryDate);
+
+    this->controlButtonArea = new QWidget();
+
+    this->unknownArea = new QWidget();
+
+    this->primaryDisplay = new QWidget();
+    this->guidewareTrackingDisplay = new QWidget();
+
+    this->vtkDisplayBoard = new QWidget();
+    this->vtkDisplayBoardLayout = new QHBoxLayout(this->vtkDisplayBoard);
+    this->vtkDisplayBoardLayout->addWidget(primaryDisplay);
+    this->vtkDisplayBoardLayout->addWidget(guidewareTrackingDisplay);
+
+    this->guideControlBoard = new QWidget();
+    this->guideControlBoard->setStyleSheet("background-color:blue");
+    this->guideControlBoardLayout = new QVBoxLayout(this->guideControlBoard);
+    this->guideControlBoardLayout->addWidget(this->surgeryInformation);
+    this->guideControlBoardLayout->addWidget(this->controlButtonArea);
+    this->guideControlBoardLayout->addWidget(this->unknownArea);
+
+    this->guidewareTrackingLayout = new QGridLayout(this);
+    this->guidewareTrackingLayout->addWidget(this->nameBar, 0, 0);
+    this->guidewareTrackingLayout->addWidget(this->vtkDisplayBar, 0, 1);
+    this->guidewareTrackingLayout->addWidget(this->guideControlBoard, 1, 0);
+    this->guidewareTrackingLayout->addWidget(this->vtkDisplayBoard, 1, 1);
+
+
+
 }
